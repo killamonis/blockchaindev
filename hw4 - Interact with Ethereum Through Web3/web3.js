b341f77f5f209d83e62a60d272f8cb4ea3ba83ae
Monis Mohiuddin <monmohiuddin@berkeley.edu>
2021-03-04 16:18:16 -0800

    finished hw4

---
+++ hw4 - Interact with Ethereum Through Web3/web3.js
@@ -0,0 +1,102 @@
+// Commands for you to execute in interactive node window
+// If you want, you can also deploy it in a simple web app
+const Web3 = require('web3')
+// Get the endpoint under the correct test net in Infura
+var web3 = new Web3(new Web3.providers.HttpProvider('https://ropsten.infura.io/v3/71db986335b3453b9edf9dc5e2913360'));
+// Get your contract's deployed address, access that through the link in Remix console.
+const address = '0xA2D76Df74637712810Af4CF073B472EBf332f209';
+// Your contract's ABI, from Remix IDE
+const ABI  = [
+	{
+		"constant": false,
+		"inputs": [],
+		"name": "deposit",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": true,
+		"stateMutability": "payable",
+		"type": "function"
+	},
+	{
+		"constant": false,
+		"inputs": [
+			{
+				"name": "withdrawAmount",
+				"type": "uint256"
+			}
+		],
+		"name": "withdraw",
+		"outputs": [
+			{
+				"name": "remainingBal",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "function"
+	},
+	{
+		"inputs": [],
+		"payable": false,
+		"stateMutability": "nonpayable",
+		"type": "constructor"
+	},
+	{
+		"anonymous": false,
+		"inputs": [
+			{
+				"indexed": false,
+				"name": "accountAddress",
+				"type": "address"
+			},
+			{
+				"indexed": false,
+				"name": "amount",
+				"type": "uint256"
+			}
+		],
+		"name": "LogDepositMade",
+		"type": "event"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "balance",
+		"outputs": [
+			{
+				"name": "",
+				"type": "uint256"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	},
+	{
+		"constant": true,
+		"inputs": [],
+		"name": "owner",
+		"outputs": [
+			{
+				"name": "",
+				"type": "address"
+			}
+		],
+		"payable": false,
+		"stateMutability": "view",
+		"type": "function"
+	}
+];
+
+
+// Get properties regarding the function calls
+web3.eth.getBalance
+// Specify your contract
+const myContract = new web3.eth.Contract(ABI, address)
+// Async call to get the contract's balance, interacting with the contract
+myContract.methods.balance().call().then(console.log)
